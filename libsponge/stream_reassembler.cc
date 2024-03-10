1955bc4a52db34561dbb250cc0b3cea85f43b02c
chseong-cs353-vm <chseong@postech.ac.kr>
2024-03-09 22:42:34 -0800

    ADD: assn1 last staging, write logic

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -75,7 +75,30 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
             temp = make_tuple(max(get<1>(*iter) + 1, temp_start), temp_end);
         }
     }
-    
+    if (iter == _staging_list.end()) {
+        size_t temp_start = get<0>(temp);
+        size_t temp_end = get<1>(temp);
+        size_t remain_size = _capacity - unassembled_bytes() - _output.buffer_size();
+        if (remain_size != 0) {
+            size_t max_end = min(
+                temp_end,
+                min(temp_start + remain_size - 1, _unassembled_start_index + _capacity - _output.buffer_size() - 1));
+            _staging_list.push_back(
+                make_tuple(temp_start, max_end, data.substr(temp_start - index, max_end - temp_start + 1)));
+            _staging_size += max_end - temp_start + 1;
+        }
+    }
+    for (iter = _staging_list.begin(); iter != _staging_list.end();) {
+        if (get<0>(*iter) == _unassembled_start_index) {
+            size_t size = get<1>(*iter) - get<0>(*iter) + 1;
+            _output.write(move(get<2>(*iter)));
+            _unassembled_start_index = get<1>(*iter) + 1;
+            iter = _staging_list.erase(iter);
+            _staging_size -= size;
+        } else {
+            break;
+        }
+    }
     _eof_process();
 }
 
